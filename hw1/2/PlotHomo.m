c40c2b65780c09b96d6211693f30069aa2f0fadf
Wander <lywander@gmail.com>
2012-01-19 17:11:55 -0800

    problem 2,3, 4(a)

--- hw1/2/PlotHomo.m
+++ hw1/2/PlotHomo.m
@@ -1,12 +1,9 @@
-function   PlotHomo( l )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-a=l(1,1);
-b=l(2,1);
-c=l(3,1);
-syms x y;
-  ezplot(a * x + b * y + c);
-  colormap([0, 0, 1]); %blue line
-
+function   PlotHomo( line )
+    a=line(1,1);
+    b=line(2,1);
+    c=line(3,1);
+    syms x y;
+    ezplot(a * x + b * y + c);
+    colormap([0, 0, 1]); %blue line
 end
 
